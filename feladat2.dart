0c1d5c8a64bd51d47e2b920f9381f0c2d43bdc6e
cstmrtn <153620020+cstmrtn@users.noreply.github.com>
2024-09-17 11:32:35 +0200

    Create feladat2.dart

---
+++ feladat2.dart
@@ -0,0 +1,13 @@
+void main() {
+List<String> uefa2024euro = [
+"Spain",
+"Germany",
+"Portugal",
+"France",
+"Netherlands",
+"Turkey",
+"England",
+"Switzerland"
+];
+uefa2024euro..asMap().forEach((index, value) => print("$value indexe: $index"));
+}
